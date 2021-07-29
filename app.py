04175d535677f3539f2b57f5b772468700e0ad9e
KAntoniuk <karol.antoniuk5@gmail.com>
2021-07-29 16:43:20 +0100

    added html templates

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, send_file, request, Response, flash
+
+
+import time
+from uk_covid19 import Cov19API
+import ipywidgets as wdg
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+#import seaborn as sns
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
-
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
